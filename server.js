c1abc15da744bc5fc6ac32ed9c3ed4bd3ac887bd
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-13 16:21:29 +0000

    oops

--- server.js
+++ server.js
@@ -5,14 +5,3 @@ const michaelsFunc = () => {
 const michaelsCoolFunc = () => {
   console.log("michaels cool func");
 };
-
-
-const adamsfunc = () => {
-  console.log("have I broke this yet?")
-}
-
-const adamfunc2 = () => {
-  console.log("will this be pushed?")
-
-const jessicasAwsomeFunc = () => {
-  console.log("This awsome Function does nothing");
