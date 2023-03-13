e931f58a5884ffc8bbd6cd270395039957c1f7e8
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-13 15:59:11 +0000

    addedMattsFunctions

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const michaelsFunc = () => {
 const michaelsCoolFunc = () => {
   console.log("michaels cool func");
 };
+
+
+
+const mattsterribleFunction = () => {
+  let func = Date();
+
+  try {
+    
+  } catch (error) {
+     res.status(501).json({ errorMessage: error.message, error: error })
+  }
+  console.log(func)
+}
