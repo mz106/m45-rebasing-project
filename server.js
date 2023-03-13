fb25a3b41e58b6ebb4ae864566bdfbe88153f9ca
K Burnett <bluemalek@gmail.com>
2023-03-13 16:02:00 +0000

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const michaelsFunc = () => {
   console.log("this is michaels func");
 };
+
+const cazadorDelaGato = () => {
+  console.log("El gato no es bueno");
+};
+
+const aquas = () => {
+  console.log("¡Que padre!");
+};
+
+const american = () => {
+  console.log("Estadounidense");
+};
